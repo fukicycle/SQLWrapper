9dce213fb2ba0531b851b8afa17bbc5766a1cf51
Fuki Sato <lego.sato.4135@gmail.com>
2022-07-08 11:50:41 +0900

    データベースの設定保持クラス追加 コネクションストリング生成メソッド追加

---
+++ SQLWrapper/DBSettins.cs
@@ -0,0 +1,16 @@
+﻿namespace SQLWrapper
+{
+    public static class DBSettings
+    {
+        public static string DB_USERNAME = "";
+        public static string DB_PASSWORD = "";
+        public static string DB_NAME = "";
+        public static string DB_HOST = "localhost";
+
+        public static string GetConnectionString()
+        {
+            if (DB_USERNAME == string.Empty || DB_PASSWORD == string.Empty || DB_NAME == string.Empty) return "Required parameter is not set.";
+            return string.Format("SERVER={0}; DATABASE={1}; UID={2}; PASSWORD={4}", DB_HOST, DB_NAME, DB_USERNAME, DB_PASSWORD);
+        }
+    }
+}
