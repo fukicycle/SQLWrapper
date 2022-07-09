0b9c5fd5685d761851d6f283cb294c3c984723cb
Fuki Sato <lego.sato.4135@gmail.com>
2022-07-09 19:18:00 +0900

    test for connection string

--- SQLWrapperTest/UnitTest1.cs
+++ SQLWrapperTest/UnitTest1.cs
@@ -5,12 +5,17 @@ namespace SQLWrapperTest
         [SetUp]
         public void Setup()
         {
+            DBSettings.DB_HOST = "localhost";
+            DBSettings.DB_NAME   = "champion_road";
+            DBSettings.DB_USERNAME = "root";
+            DBSettings.DB_PASSWORD = "P@ssWord";
         }
 
         [Test]
-        public void Test1()
+        [Order(0)]
+        public void GetConnectionStringTest()
         {
-            Assert.Pass();
+            Assert.IsTrue(DBSettings.GetConnectionString() == $"SERVER={DBSettings.DB_HOST}; DATABASE={DBSettings.DB_NAME}; UID={DBSettings.DB_USERNAME}; PASSWORD={DBSettings.DB_PASSWORD};");
         }
     }
 }
