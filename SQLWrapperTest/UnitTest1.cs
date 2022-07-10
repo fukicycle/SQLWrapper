4911fbf56aad4f4bc964f41979f50cbeb6fe3233
Fuki Sato <lego.sato.4135@gmail.com>
2022-07-10 22:54:55 +0900

    シングルトンパターン インスタンスのテスト

--- SQLWrapperTest/UnitTest1.cs
+++ SQLWrapperTest/UnitTest1.cs
@@ -5,17 +5,30 @@ namespace SQLWrapperTest
         [SetUp]
         public void Setup()
         {
-            DBSettings.DB_HOST = "localhost";
-            DBSettings.DB_NAME   = "champion_road";
-            DBSettings.DB_USERNAME = "root";
+            DBSettings.DB_HOST = "192.168.0.101";
+            DBSettings.DB_NAME = "champion_road";
+            DBSettings.DB_USERNAME = "dev";
             DBSettings.DB_PASSWORD = "P@ssWord";
         }
 
         [Test]
-        [Order(0)]
+        [Order(1)]
         public void GetConnectionStringTest()
         {
             Assert.IsTrue(DBSettings.GetConnectionString() == $"SERVER={DBSettings.DB_HOST}; DATABASE={DBSettings.DB_NAME}; UID={DBSettings.DB_USERNAME}; PASSWORD={DBSettings.DB_PASSWORD};");
+        }
+        [Test]
+        [Order(2)]
+        public void ConnectTest()
+        {
+            Assert.IsTrue(MysqlWrapper.Connect());
+        }
+
+        [Test]
+        [Order(3)]
+        public void GetInstanceTest()
+        {
+            Assert.IsInstanceOf<MysqlWrapper>(MysqlWrapper.GetInstance());
         }
     }
 }
